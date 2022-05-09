39e1a7cf1003e23915dfe91112770749db9bc3d8
Prajwal Jaiswal <prajwalmjaiswal@gmail.com>
2022-05-09 21:52:10 +0530

    Create a temp api

--- server/controller/users.js
+++ server/controller/users.js
@@ -1,4 +1,5 @@
 import User from "../models/users.js";
+import Post from "../models/posts.js";
 
 export const getAllUsers = async (req, res) => {
   try {
@@ -37,13 +38,24 @@ export const userLogin = async (req, res) => {
   try {
     const users = await User.find({ email: req.body.email });
     if (users.length === 0) {
-      return res.status(404).json({ message: "Cannot find User with this email" });
+      return res
+        .status(404)
+        .json({ message: "Cannot find User with this email" });
     }
     if (users[0].password === req.body.password) {
       res.send(users[0]);
     } else {
       res.status(403).json({ message: "Invalid credenteials" });
     }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    // to be used in the user's profile page
+    // to show all the posts the user has
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
