2325bf982e2fc5c17f37633c32219c7860c8c972
Prajwal Jaiswal <prajwalmjaiswal@gmail.com>
2022-05-10 09:12:52 +0530

    Add links in the left panel

--- client/src/components/leftPanel/LeftPanel.jsx
+++ client/src/components/leftPanel/LeftPanel.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { GoVerified } from "react-icons/go";
+import { HiHashtag, HiBell } from "react-icons/hi";
+import { FaUserAlt } from "react-icons/fa";
+import { BiCalendarEvent } from "react-icons/bi";
+import { BsFillBookmarkFill } from "react-icons/bs";
 
 import "./LeftPanel.css";
-import MyEvents from "./myEvents/MyEvents"
+import MyEvents from "./myEvents/MyEvents";
 
 const LeftPanel = ({ user, updateUser }) => {
   return (
@@ -27,11 +32,27 @@ const LeftPanel = ({ user, updateUser }) => {
         </p>
       </div>
       <div className="leftPanelItems">
-        <div className="myEvents">
+        {/* <div className="myEvents">
           <MyEvents />
-        </div>
+        </div> */}
+        <Link to="/" className="leftPanelLinks">
+          <FaUserAlt className="leftPanelIcon userIcon" /> Profile
+        </Link>
+        <Link to="/" className="leftPanelLinks">
+          <HiBell className="leftPanelIcon bellIcon" /> Notifications
+        </Link>
+        <Link to="/" className="leftPanelLinks">
+          <HiHashtag className="leftPanelIcon exploreIcon" /> Explore
+        </Link>
+        <Link to="/" className="leftPanelLinks">
+          <BiCalendarEvent className="leftPanelIcon eventsIcon" /> My Events
+        </Link>
+        <Link to="/" className="leftPanelLinks">
+          <BsFillBookmarkFill className="leftPanelIcon saveIcon" /> Saved Items
+        </Link>
         <button onClick={() => updateUser({})}>Logout</button>
       </div>
+      <div className="more">Discover more</div>
     </div>
   );
 };
